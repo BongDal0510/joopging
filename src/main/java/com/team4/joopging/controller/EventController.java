76a0b0e4cf842b6fa208c5498e645f2a5bda813a
celly <eugenee11101@gmail.com>
2021-10-27 23:11:41 +0900

    event omission resources rename cleanup commit

--- src/main/java/com/team4/joopging/controller/EventController.java
+++ src/main/java/com/team4/joopging/controller/EventController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 @RequestMapping("/event/*")
 @RequiredArgsConstructor
-public class Event22Controller {
+public class EventController {
 
     @GetMapping("eventlist")
     public String event() {
