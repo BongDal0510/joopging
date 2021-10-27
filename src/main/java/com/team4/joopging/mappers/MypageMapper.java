2de4c2da48c6dc412d61f905c9c3f1b2704be481
by <daydayto@naver.com>
2021-10-27 20:24:22 +0900

    MypageMapper commit 01

--- src/main/java/com/team4/joopging/mappers/MypageMapper.java
+++ src/main/java/com/team4/joopging/mappers/MypageMapper.java
@@ -9,15 +9,15 @@ import java.util.List;
 @Mapper
 public interface MypageMapper {
     /*플로깅 예약 목록*/
-    public List<reservationVO> getReservationList();
+//    public List<reservationVO> getReservationList();
     /*포인트 목록*/
     public List<pointVO> getPointList();
     /*구매 상품 목록*/
-    public List<goodsOrderVO> getGoodsOrderList();
+//    public List<goodsOrderVO> getGoodsOrderList();
     /*상품의 택배*/
     public parcelVO getParcel();
     /*찜 목록*/
-    public List<goodsLikeListVO> getGoodsLikeList();
+//    public List<goodsLikeListVO> getGoodsLikeList();
     /*내가 작성한 게시판 목록*/
     public List<CommuVO> getMyCommuList();
     /*내가 작성한 1:1문의 목록*/
