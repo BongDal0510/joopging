4bea459528831f9395639d3299108ced9e4025b2
celly <eugenee11101@gmail.com>
2021-10-27 17:09:59 +0900

    new 'headerfooter' branch related commit

--- src/main/java/com/team4/joopging/controller/MainpageController.java
+++ src/main/java/com/team4/joopging/controller/MainpageController.java
@@ -12,34 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class MainpageController {
 
-    @GetMapping("mainpage")
-    public String mainpage() {
-        return "mainpage";
-    }
-
-    @GetMapping("cs")
-    public String cs() {
-        return "cs";
-    }
-
-    @GetMapping("/pageframe/header")
+    @GetMapping("header")
     public String header() {
-        return "/pageframe/header";
+        return "pageframe/header";
     }
 
-    @GetMapping("/pageframe/footer")
+    @GetMapping("footer")
     public String footer() {
-        return "/pageframe/footer";
-    }
-
-    @GetMapping("eventInfo3")
-    public String eventInfo3() {
-        return "eventInfo3";
-    }
-
-    @GetMapping("eventInfo4")
-    public String eventInfo4() {
-        return "eventInfo4";
+        return "pageframe/footer";
     }
 
 }
