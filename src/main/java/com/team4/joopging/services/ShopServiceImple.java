3fc1ff02c1fda1efae88c2bda88a7c00be6448c5
wonjun <wonjun775@naver.com>
2021-10-29 13:40:32 +0900

    샵 service까지 완료

---
+++ src/main/java/com/team4/joopging/services/ShopServiceImple.java
@@ -0,0 +1,40 @@
+package com.team4.joopging.services;
+
+import com.team4.joopging.beans.dao.ShopDAO;
+import com.team4.joopging.beans.vo.ShopVO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ShopServiceImple implements ShopService{
+
+    private final ShopDAO shopDAO;
+
+    @Override
+    public void goodsRegister(ShopVO shop) {
+        shopDAO.goodsRegister(shop);
+    }
+
+    @Override
+    public ShopVO goodsGet(Long goodsNum) {
+        return shopDAO.goodsGet(goodsNum);
+    }
+
+    @Override
+    public boolean goodsModify(ShopVO shop) {
+        return shopDAO.goodsModify(shop);
+    }
+
+    @Override
+    public boolean goodsRemove(Long goodsNum) {
+        return shopDAO.goodsDelete(goodsNum);
+    }
+
+    @Override
+    public List<ShopVO> goodsGetList() {
+        return shopDAO.goodsGetList();
+    }
+}
---
+++ src/test/java/com/team4/joopging/services/ShopServiceTest.java
@@ -0,0 +1,30 @@
+package com.team4.joopging.services;
+
+import com.team4.joopging.beans.vo.ShopVO;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestExecutionListeners;
+
+@SpringBootTest
+@Slf4j
+public class ShopServiceTest {
+    @Autowired
+    private ShopService shopService;
+
+    @Test
+    public void testGoodsRegister(){
+        ShopVO shop = new ShopVO();
+        shop.setGoodsName("수정 테스트 상품7");
+        shop.setGoodsCategory("T-shirts");
+        shop.setGoodsPrice("5,000");
+        shop.setGoodsContent("수정 테스트 상품7 입니다.");
+
+        shopService.goodsRegister(shop);
+    }
+    @Test
+    public void testGetGoodsList(){
+        shopService.goodsGetList().forEach(shop-> log.info(shop.toString()));
+    }
+}
