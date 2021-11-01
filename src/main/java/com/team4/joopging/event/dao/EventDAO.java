ae0a37c306b6a8abe8d2d2cff40a8da403c5e2cd
celly <eugenee11101@gmail.com>
2021-11-01 17:32:09 +0900

    mappers package test related commit

---
+++ src/main/java/com/team4/joopging/event/dao/EventDAO.java
@@ -0,0 +1,9 @@
+package com.team4.joopging.event.dao;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class EventDAO {
+}
---
+++ src/main/java/com/team4/joopging/mappers/EventMapper.java
@@ -0,0 +1,7 @@
+package com.team4.joopging.mappers;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface EventMapper {
+}
