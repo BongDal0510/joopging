07b4126758f341907f7f93ae24ec48c956f04205
gkdbs1009 <gkdbs1009@naver.com>
2021-11-03 19:38:31 +0900

    2021-11-03 성윤
    user 브랜치 업데이트

---
+++ src/main/java/com/team4/joopging/mappers/MemberMapper.java
@@ -0,0 +1,30 @@
+package com.team4.joopging.mappers;
+
+import com.team4.joopging.member.memberVO.MemberVO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MemberMapper {
+    /*아이디 중복 체크*/
+    public int idCheck(MemberVO memberVO);
+    /*회원 가입*/
+    public void join(MemberVO memberVO);
+    /*카카오 회원 가입*/
+    public void joinKAKAO(MemberVO memberVO);
+    /*네이버 회원 가입*/
+    public void joinNAVER(MemberVO memberVO);
+    /*로그인*/
+    public int login(MemberVO memberVO);
+    /*카카오로그인*/
+    public int loginKAKAO(MemberVO memberVO);
+    /*네이버로그인*/
+    public int loginNAVER(MemberVO memberVO);
+    /*아이디 찾기*/
+    public String searchId(MemberVO memberVO);
+    /*비밀번호 검색*/
+    public String searchPw(MemberVO memberVO);
+    /*비밀번호 변경*/
+    public void updatePw(MemberVO memberVO);
+    /*회원정보 전체 조회*/
+    public MemberVO allSelect(MemberVO memberVO);
+}
