8b6d042783b8cc2337f817443aea7794ead2f94b
alexiaH <alexiahwang@gmail.com>
2021-11-04 10:07:04 +0900

    2021-11-04 JW report.html 신고하기 버튼 클릭시 팝업될 페이지 완성 communityRead.html 신고하기 버튼에 경로 연결함

---
+++ src/main/java/com/team4/joopging/mappers/ReportMapper.java
@@ -0,0 +1,37 @@
+package com.team4.joopging.mappers;
+
+import com.team4.joopging.community.vo.CommuVO;
+import com.team4.joopging.community.vo.Criteria;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface ReportMapper {
+    //게시글 목록
+    public List<CommuVO> getCommuList(Criteria criteria);
+
+    //게시글 등록
+    public void insertCommu(CommuVO commu);
+
+    //등록한 게시글의 PK가져오기 = bno
+    public void insertSelectKey_commuBno(CommuVO commu);
+
+    //게시글 상세보기
+    public CommuVO readCommu(Long commuBno);
+
+    //게시글 수정
+    public int updateCommu(CommuVO commu);
+
+    //게시글 삭제
+    public int deleteCommu(Long commuBno);
+
+    //게시글 총 갯수
+    public int getCommuTotal(Criteria criteria);
+
+    //게시글 조회수
+    public int updateCommuViewCnt(Long commuBno);
+
+    //공지글 가져오기
+    public List<CommuVO> getAnnounceList(int commuBoardStatus);
+}
