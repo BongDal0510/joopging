76cd1a8d5c1dd237d645ba794d3005027e6216b8
celly <eugenee11101@gmail.com>
2021-11-06 03:16:45 +0900

    2021-11-06 YJ
    포인트 브랜치
    메소드 매개변수
    memberNum > memberId로 변경

--- src/main/java/com/team4/joopging/point/dao/PointDAO.java
+++ src/main/java/com/team4/joopging/point/dao/PointDAO.java
@@ -15,13 +15,13 @@ public class PointDAO {
     private final PointMapper mapper;
 
     /*포인트 내역 출력(넘버로)*/
-    public List<PointVO> getPointList(int memberNum, Criteria criteria){
-        return mapper.getPointList(memberNum, criteria);
+    public List<PointVO> getPointList(String memberId, Criteria criteria){
+        return mapper.getPointList(memberId, criteria);
     }
 
     /*포인트 전체 개수*/
-    public int totalPointCnt(int memberNum){
-        return totalPointCnt(memberNum);
+    public int totalPointCnt(String memberId){
+        return totalPointCnt(memberId);
     };
 
     /*포인트 추가*/
@@ -29,9 +29,9 @@ public class PointDAO {
         return mapper.addPoint(vo) == 1;
     }
 
-    /*구매 상품 취소()*/
+    /*구매 상품 취소*/
     public boolean removeOrder(PointVO vo){
-        return mapper.removeOrder(vo)==1;
+        return mapper.removeOrder(vo) == 1;
     }
 
 
