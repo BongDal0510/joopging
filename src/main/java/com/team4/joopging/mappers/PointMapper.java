bbd03aa9973fc7cffec0841e0160429eaada036f
celly <eugenee11101@gmail.com>
2021-11-06 01:42:13 +0900

    2021-11-06 YJ
    포인트 브랜치 분리 작업

---
+++ src/main/java/com/team4/joopging/mappers/PointMapper.java
@@ -0,0 +1,26 @@
+package com.team4.joopging.mappers;
+
+import com.team4.joopging.community.vo.Criteria;
+import com.team4.joopging.point.vo.PointVO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface PointMapper {
+    /*포인트 목록*/
+    public List<PointVO> getPointList(@Param("memberNum")int memberNum, @Param("criteria")Criteria criteria);
+
+    /*회원의 포인트 전체 내역 개수 */
+    public int totalPointCnt(int memberNum);
+
+    /*포인트 추가*/
+    public int addPoint(PointVO vo);
+
+    /*구매 상품 취소*/
+    public int removeOrder(PointVO vo);
+
+    /*구매 취소된 상품 인지 확인 여부*/
+    public int checkDeletOrder(int orderNum);
+}
---
+++ src/main/java/com/team4/joopging/services/PointService.java
@@ -0,0 +1,22 @@
+package com.team4.joopging.services;
+
+import com.team4.joopging.community.vo.Criteria;
+import com.team4.joopging.point.vo.PointVO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface PointService {
+
+    /*포인트 내역 출력(넘버로)*/
+    public List<PointVO> getPointList(int memberNum, Criteria criteria);
+
+    /*포인트 전체 개수(내역 페이지 이동에 사용)*/
+    public int totalPointCnt(int memberNum);
+
+    /*포인트 추가*/
+    public boolean addPoint(PointVO vo);
+
+
+}
