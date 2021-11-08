c7b84908eae15879546b42263f80a1e8b1318b70
wonjun <wonjun775@naver.com>
2021-11-09 08:27:29 +0900

    RestartShop 작업 16차(admin 넣어보는중2)

--- src/main/java/com/team4/joopging/controller/AdminController.java
+++ src/main/java/com/team4/joopging/controller/AdminController.java
@@ -15,4 +15,18 @@ public class AdminController {
     @GetMapping("admin")
     public String adminMain() { return "/admin/admin"; }
 
+    //헤더
+    @GetMapping("/pageframe/header")
+    public String header() {
+        return "/pageframe/header";
+    }
+
+    //푸터
+    @GetMapping("/pageframe/footer")
+    public String footer() {
+        return "/pageframe/footer";
+    }
+
+
 }
+
